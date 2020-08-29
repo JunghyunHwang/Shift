32c887e90975d7440571fcf0168f13e8198af32f
Ja_Hwang <dmagk560@gmail.com>
2020-08-29 17:59:42 +0900

    S

--- pick.js
+++ pick.js
@@ -1,12 +1,19 @@
-let maxScore = 8; 
-const maxCnt = 3;
-const minCnt = 1;
-class member {
-    constructor(name, score, cnt){
+// const maxCnt = 3;
+// const minCnt = 1;
+// let maxScore = 8; 
+let member = new Array();
+let memberTotal = member.length;
 
-    }
+let cctvShift = new Array();
+for(let i = 0; i < 10; i++) { // 10 개의 cctv 근무 만듬
+    cctvShift[i] = new Object();
+    cctvShift[i].score = 1;
+    cctvShift[i].count = 1;
 }
 
-function pick() {
-    let 
+let nightShift = new Array();
+for(let i = 0; i < 4; i++) { // 4개의 불침번 근무 만듬
+    nightShift[i] = new Object();
+    nightShift[i].score = 1;
+    nightShift[i].count = 1;
 }
