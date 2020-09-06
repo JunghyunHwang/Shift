88b395ee09d38e461a77ed39a6635c071aaf59d7
Ja_Hwang <dmagk560@gmail.com>
2020-09-06 16:01:18 +0900

    Complete shift

--- box.js
+++ box.js
@@ -10,34 +10,6 @@ let memberTotal = 12; // 나중에 엑셀 연결 해서 가져옴
 let memberCnt = 0;
 let member = new Array(memberTotal);
 
-function Shift(score, cnt) // 파라미터 이름 추가
-{
-    this.score = score;
-    this.count = cnt;
-}
-
-for(let i = 0; i < nightShift.length; i++)
-{
-    nightShift[i] = new Array(numberNightShift); // nightShift[7][4]
-    let tempScore = 2; // 근무 별 점수가 3번째 근무 까지 순서대로 2,3,4 이기 때문에 변수 생성
-
-    for(let j = 0; j < numberNightShift; j++) // 근무 별 점수와 횟수 초기화
-    {
-        nightShift[i][j] = new Shift(tempScore, 1);
-        tempScore++; // 근무 별 점수가 3번째 근무 까지 순서대로 2,3,4 이기 때문에 이렇게 작성
-    }
-    nightShift[i][3].score = 3; // 불침번 마지막 근무만 점수 따로 수정
-} 
-// *** 불침번 근무 생성 및 근무 별 점수 초기화
-
-for(let i = 0; i < memberTotal; i++)
-{
-    member[i] = i;
-    member[i] = new Shift(0, 0);
-    console.log(member[i].score);
-}
-// *** 멤버들의 점수와 근무 횟수 초기화
-
 function pickNum(shiftNum)
 {
     let randomPersonNum = [];
@@ -87,51 +59,99 @@ for(let i = 0; i < weekday; i++)
 
 console.log(member);
 
-let cctv = new Array();
-for(let i = 4; i <= 22; i += 2) { // 10개의 cctv 근무 만들고 score를 1로 초기화
-    cctv[i] = new Shift(1);
+function pickNum (availableNum, maxNum)
+{
+    let randomNum = new Array(availableNum);
+    for(let i = 0; i < availableNum; i++)
+    {
+        randomNum[i] = Math.floor(Math.random() * maxNum - 1) + 1; // Pick random number
+        for(let j = 0; j < i; j++)
+        {
+            if(randomNum[i] == randomNum[j]) // 중복 제거
+            {
+                i--;
+                break;
+            }
+        }
+    }
+    
+    return randomNum;
 }
 
-//cctv 근무 별 점수
-cctv[4].score = 3;
-cctv[6].score = 2;
-cctv[14].score = 0.5;
-cctv[18].score = 4;
-cctv[20].score = 2;
-cctv[22].score = 2;
+//////////////////////////////////////////
 
+if(!fivePointMembers.length)
+{
+    point = 5;
+}
+else if(!onePointMembers.length)
+{
+    point = 1;
+}
 
+//////////////////////////////////////////
 
+if(!fivePointMembers.length || !onePointMembers.length)
+{
+    keepShift[keepShiftCount] = i;
+    keepShift[keepShiftCount][keepShiftCount] = j;
+    keepShiftCount++;
+    break;
+}
 
+if(zeroPointMembers.length) // 근무를 한번도 안 들어간 사람이 있을때
+{
+    selectedNum = pickNum(zeroPointMembers.length);
+    zeroPointMembers[selectedNum].score += point;
+    zeroPointMembers[selectedNum].count++;
+    onePointMembers.push(zeroPointMembers[selectedNum]);
+    zeroPointMembers.splice(selectedNum, 1);
+}
+else if(point != 1) // 5점 근무 일때
+{
+    if(!onePointMembers.length || !fivePointMembers) // 모든 인원이 두번씩 했을때
+    {
+        
+    }
+    else
+    {
+        selectedNum = pickNum(onePointMembers.length);
+        onePointMembers[selectedNum].score += point;
+        onePointMembers[selectedNum].count++;
+        doNotWorkMemebers.push(onePointMembers[selectedNum]);
+        onePointMembers.splice(selectedNum, 1);
+    }
+}
+else if(point != 5) // 1점 근무 일때
+{
+    if(!fivePointMembers.length || !onePointMembers.length)
+    {
 
-
-
-
-// for(let i = 0; i < week; i++)
-// {
-//     let numberNightShift = 4; // 불침번 근무 개수
-//     nightShift[i] = new Array(numberNightShift); // nightShift[7][4]
-//     for(let j = 0; j < numberNightShift; j++)
-//     {
-//         if(i < weekday) // 평일 근무 점수
-//         {
-//             nightShift[i][j] = new work(5, 1);
-//         }
-//         else // 주말 근무 점수
-//         {
-//             nightShift[i][j] = new work(0, 1);
-//         }
-//     }
-// }
-// // *** 불침번 근무 생성 및 근무 별 점수 초기화
-
-// for(let i = 0; i < week; i++)
-// {
-//     let numberCctvShift = 10; // cctv 근무 개수
-//     cctvShift[i] = new Array(numberCctvShift);
-//     for(let j = 0; j < numberCctvShift; j++)
-//     {
-//         cctvShift[i][j] = new work(0, 1);
-//     }
-// }
-// // *** cctv 근무 생성 및 근무 별 점수 초기화
+    }
+    else
+    {
+        selectedNum = pickNum(fivePointMembers.length);
+        fivePointMembers[selectedNum].score += point;
+        fivePointMembers[selectedNum].count++
+        doNotWorkMemebers.push(fivePointMembers[selectedNum]);
+        fivePointMembers.splice(selectedNum, 1);
+    }
+}
+///////////////////////////////////////////////////////////////
+else if(!fivePointMembers.length && !onePointMembers.length) // 모든 인원이 두번씩 했을때
+{
+    selectedNum = pickNum(sixPointMembers.length);
+    sixPointMembers[selectedNum].score += point;
+    sixPointMembers[selectedNum].count++;
+    doNotWorkMemebers.push(sixPointMembers[selectedNum]);
+    sixPointMembers.splice(selectedNum, 1);
+}
+//////////////////////////////////////////
+else if(!onePointMembers.length && !fivePointMembers.length) // 모든 인원이 두번씩 했을때
+{
+    selectedNum = pickNum(sixPointMembers.length);
+    sixPointMembers[selectedNum].score += point;
+    sixPointMembers[selectedNum].count++;
+    doNotWorkMemebers.push(sixPointMembers[selectedNum]);
+    sixPointMembers.splice(selectedNum, 1);
+}
--- picktest.js
+++ picktest.js
@@ -1,45 +1,200 @@
 let weekday = 5;
-let weekend = 2;
 let week = 7;
 let shift = [];
+let shiftName = ["04", "06", "08", "10", "12", "14", "16", "18", "20", "22", "first", "second", "third", "fourth", "guardhouse1", "guardhouse2"];
 let members = []; // 나중에 엑셀 연결 해서 가져옴
-let memberTotal = members.length;
+let zeroPointMembers = [];
+let onePointMembers = [];
+let fivePointMembers = [];
+let sixPointMembers = [];
+let hardWorkMembers = [];
+let doNotWorkMemebers = [];
+let shiftsTotal = 0;
 
-function work(score, cnt) // 파라미터 이름 추가
+function work(name, score, cnt)
 {
+    this.name = name
     this.score = score;
     this.count = cnt;
+}
+
+function loadMembers()
+{
+    for(let i = 0; i < 46; i++)
+    {
+        members[i] = i;
+        members[i] = new work(i, 0, 0);
+    }
+}
+
+function memberCategory (total)
+{
+    for(let i = 0; i < total; i++)
+    {
+        let point = members[i].score;
+
+        switch(point)
+        {
+            case 0 :
+                zeroPointMembers.push(members[i]);
+                break;
+            case 1 :
+                onePointMembers.push(members[i]);
+                break;
+            case 5 :
+                fivePointMembers.push(members[i]);
+                break;
+            // default :
+            //     doNotWorkMemebers.push(members[i]); //////////////////이거 한번 확인
+            //     break;
+        }
+    }
 }
 
 for(let i = 0; i < week; i++)
 {
     let numberNightShift = 4; // 불침번 근무 개수
     let numberCctvShift = 10; // cctv 근무 개수
-    let shiftsTotal = numberNightShift + numberCctvShift
+    shiftsTotal = numberNightShift + numberCctvShift // onedayShiftTotal
     shiftsTotal = (i < weekday) ? shiftsTotal : shiftsTotal + 2; // 주말 위병소 근무 때문에
     shift[i] = new Array(shiftsTotal);
-    for(let j = 0; j < shiftsTotal; j++) // 모든 근무 점수 카운트 초기화
+    
+    for(let j = 0; j < shiftsTotal; j++)
     {
-        shift[i][j] = new work(0, 1);
+        if(j == 7)// shift[i][7]는 18~20이고, 매일 5점 짜리 근무임
+        {
+            shift[i][j] = new work(shiftName[j], 5, 1);
+        }
+        else
+        {
+            shift[i][j] = new work(shiftName[j], 1, 1); // 모든 근무 점수, 카운트 0으로 초기화
+        }
     }
-    for(let k = 10; k < shiftsTotal; k++) // 불침번 근무 점수 설정
+
+    for(let k = 10; k < shiftsTotal; k++) // 불침번 점수 설정
     {
-        shift[i][k].score = (i == 4 || i == 5) ? 0 : 5; // 금,토 불침번은 0점 으로 설정
+        shift[i][k].score = (i == 4 || i == 5) ? 1 : 5; // 금,토 불침번은 0점 으로 설정
     }
-    shift[i][7].score = 5; // shift[i][7]는 18~20이고, 매일 5점 짜리 근무임
+
+    if(i >= weekday) // 주말 cctv 점수 설정
+    {
+        for(let l = 2; l < 7; l++)
+        {
+            shift[i][l].score = 5;
+        }
+        
+    }
+
+    shift[i][0].score = (i < weekday) ? 5 : 1; // cctv 초번 점수 설정
+    shift[i][9].score = (i < weekday - 1) ? 5 : 1; // cctv 말번 월~목 까지 5점으로 설정
 }
+shift[6][9].score = 5; // 일요일 cctv 말번
+shift[5][14].score = 5;
+shift[5][15].score = 5;
+
 console.log(shift);
 
-function loadMembers()
+// *** 근무 짜기 전 멤버들과 근무 설정
+
+function pickNum (maxNum)
 {
-    for(let i = 0; i < memberTotal; i++)
+    let randomNum = Math.floor(Math.random() * maxNum - 1) + 1; // Pick random number
+    return randomNum;
+}
+
+loadMembers();
+let membersTotal = members.length;
+memberCategory(membersTotal);
+
+for(let i = 0; i < week; i++)
+{
+    let numberNightShift = 4;
+    let numberCctvShift = 10;
+    shiftsTotal = numberNightShift + numberCctvShift;
+    shiftsTotal = (i < weekday) ? shiftsTotal : shiftsTotal + 2;
+    
+    for(let j = 0; j < shiftsTotal; j++)
     {
-        members[i] = i;
-        members[i] = new work(0, 0);
+        let point = shift[i][j].score;
+        let selectedNum = 0;
+
+        switch(point)
+        {
+            case 1:
+                if(zeroPointMembers.length)
+                {
+                    selectedNum = pickNum(zeroPointMembers.length);
+                    zeroPointMembers[selectedNum].score += point;
+                    zeroPointMembers[selectedNum].count++;
+                    onePointMembers.push(zeroPointMembers[selectedNum]);
+                    zeroPointMembers.splice(selectedNum, 1);
+                }
+                else if(!fivePointMembers.length)
+                {
+                    if(!sixPointMembers.length)
+                    {
+                        selectedNum = pickNum(hardWorkMembers.length);
+                        hardWorkMembers[selectedNum].score += point;
+                        hardWorkMembers[selectedNum].count++
+                        doNotWorkMemebers.push(hardWorkMembers[selectedNum]);
+                        hardWorkMembers.splice(selectedNum, 1);
+                    }
+                    else
+                    {
+                        selectedNum = pickNum(sixPointMembers.length);
+                        sixPointMembers[selectedNum].score += point;
+                        sixPointMembers[selectedNum].count++
+                        hardWorkMembers.push(sixPointMembers[selectedNum]);
+                        sixPointMembers.splice(selectedNum, 1);
+                    }
+                }
+                else // fivePointMembers
+                {
+                    selectedNum = pickNum(fivePointMembers.length);
+                    fivePointMembers[selectedNum].score += point;
+                    fivePointMembers[selectedNum].count++
+                    sixPointMembers.push(fivePointMembers[selectedNum]);
+                    fivePointMembers.splice(selectedNum, 1);
+                }
+                break;
+            case 5:
+                if(zeroPointMembers.length)
+                {
+                    selectedNum = pickNum(zeroPointMembers.length);
+                    zeroPointMembers[selectedNum].score += point;
+                    zeroPointMembers[selectedNum].count++;
+                    fivePointMembers.push(zeroPointMembers[selectedNum]);
+                    zeroPointMembers.splice(selectedNum, 1);
+                }
+                else if(!onePointMembers.length)
+                {
+                    if(!sixPointMembers.length)
+                    {
+                        selectedNum = pickNum(hardWorkMembers.length);
+                        hardWorkMembers[selectedNum].score += point;
+                        hardWorkMembers[selectedNum].count++
+                        doNotWorkMemebers.push(hardWorkMembers[selectedNum]);
+                        hardWorkMembers.splice(selectedNum, 1);
+                    }
+                    else
+                    {
+                        selectedNum = pickNum(sixPointMembers.length);
+                        sixPointMembers[selectedNum].score += point;
+                        sixPointMembers[selectedNum].count++
+                        doNotWorkMemebers.push(sixPointMembers[selectedNum]);
+                        sixPointMembers.splice(selectedNum, 1);
+                    }
+                }
+                else // onePointMembers
+                {
+                    selectedNum = pickNum(onePointMembers.length);
+                    onePointMembers[selectedNum].score += point;
+                    onePointMembers[selectedNum].count++;
+                    sixPointMembers.push(onePointMembers[selectedNum]);
+                    onePointMembers.splice(selectedNum, 1);
+                }
+                break;
+        }
     }
 }
-loadMembers();
-
-// *** 멤버들의 점수와 근무 횟수 초기화
-
-
+console.log(members);
