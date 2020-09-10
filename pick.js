adaf5902b3a97365cc05d016cbea619c59ab9916
Ja_Hwang <dmagk560@gmail.com>
2020-09-10 15:16:04 +0900

    Dodge 1.0.0

--- pick.js
+++ pick.js
@@ -1,5 +1,6 @@
 let weekday = 5;
 let week = 7;
+let shiftCount = 0;
 let shift = [];
 let members = []; // 나중에 엑셀 연결 해서 가져옴
 let zeroPointMembers = [];
@@ -9,8 +10,7 @@ let sixPointMembers = [];
 let hardWorkMembers = [];
 let doNotWorkMemebers = [];
 let shiftsTotal = 0;
-let monday = document.getElementById('mon');
-monday.innerText = "Fuck";
+let person = ["길윤재", "황중현", "이근혁", "서동휘", "나종원", "박시현", "이관진", "임석범", "황인성", "류희성", "복병수", "김민수", "김종훈", "김정현", "백지용", "홍우진", "김민수2", "김수환", "김승진", "손건우", "강우석", "송강산", "김석희", "김선규", "박태규", "공민식", "오도경", "홍성원", "최현준", "권오복", "최재성", "김창민", "이영한" , "박준서", "김수원", "김건호", "강건호", "김정원"];
 
 function work(name, score, cnt, day)
 {
@@ -20,66 +20,33 @@ function work(name, score, cnt, day)
     this.day = day;
 }
 
-function loadMembers()
-{
-    for(let i = 0; i < 28; i++)
-    {
-        members[i] = i;
-        members[i] = new work(i, 0, 0, 0);
-    }
-}
-
-function memberCategory (total)
-{
-    for(let i = 0; i < total; i++)
-    {
-        let point = members[i].score;
-
-        switch(point)
-        {
-            case 0 :
-                zeroPointMembers.push(members[i]);
-                break;
-            case 1 :
-                onePointMembers.push(members[i]);
-                break;
-            case 5 :
-                fivePointMembers.push(members[i]);
-                break;
-            // default :
-            //     doNotWorkMemebers.push(members[i]); //////////////////이거 한번 확인
-            //     break;
-        }
-    }
-}
-
 for(let i = 0; i < week; i++)
 {
     let numberNightShift = 4; // 불침번 근무 개수
     let numberCctvShift = 10; // cctv 근무 개수
     let shiftName = ["04", "06", "08", "10", "12", "14", "16", "18", "20", "22", "first", "second", "third", "fourth", "guardhouse1", "guardhouse2"];
-    shiftsTotal = numberNightShift + numberCctvShift // onedayShiftTotal
+    shiftsTotal = numberNightShift + numberCctvShift
     shiftsTotal = (i < weekday) ? shiftsTotal : shiftsTotal + 2; // 주말 위병소 근무 때문에
     shift[i] = new Array(shiftsTotal);
     
     for(let j = 0; j < shiftsTotal; j++)
     {
-        if(j == 7)// shift[i][7]는 18~20이고, 매일 5점 짜리 근무임
+        if(j == 7) // shift[i][7]는 18~20이고, 매일 5점 짜리 근무임
         {
             shift[i][j] = new work(shiftName[j], 5, 1, i);
         }
         else
         {
-            shift[i][j] = new work(shiftName[j], 1, 1, i); // 모든 근무 점수, 카운트 0으로 초기화
+            shift[i][j] = new work(shiftName[j], 1, 1, i); // 모든 근무 점수,카운트 1로 초기화
         }
     }
 
-    for(let k = 10; k < shiftsTotal; k++) // 불침번 점수 설정
+    for(let k = 10; k < shiftsTotal; k++) // 불침번 점수 설정 set nightshift score
     {
-        shift[i][k].score = (i == 4 || i == 5) ? 1 : 5; // 금,토 불침번은 0점 으로 설정
+        shift[i][k].score = (i == 4 || i == 5) ? 1 : 5; // 금,토 불침번은 1점 으로 설정 fri, sat nightshift score is 1
     }
 
-    if(i >= weekday) // 주말 cctv 점수 설정
+    if(i >= weekday) // 주말 cctv 점수 설정 set weekend cctv score
     {
         for(let l = 2; l < 7; l++)
         {
@@ -92,43 +59,69 @@ for(let i = 0; i < week; i++)
     shift[i][9].score = (i < weekday - 1) ? 5 : 1; // cctv 말번 월~목 까지 5점으로 설정
 }
 shift[6][9].score = 5; // 일요일 cctv 말번
-shift[5][14].score = 5;
-shift[5][15].score = 5;
+shift[5][14].score = 5; // saturday guardhouse
+shift[5][15].score = 5; // saturday guardhouse
 
-// *** 근무 짜기 전 멤버들과 근무 설정
+//*** 근무 및 인원 초기화 끝
 
-loadMembers();
-let membersTotal = members.length;
-memberCategory(membersTotal);
-let shiftCount = 0; ////
-
-function pickNum (maxNum)
+function loadMembers()
 {
-    let randomNum = Math.floor(Math.random() * maxNum - 1) + 1; // Pick random number
-    return randomNum;
+    for(let i = 0; i < person.length; i++)
+    {
+        members[i] = new work(person[i], 0, 0, 0);
+        zeroPointMembers[i] = members[i];
+    }
 }
 
-// day에 정보를 담아서 보내는거야 예를 들어 총 인원이 몇 
+function checkMembersDay(here, day)
+{
+    let count = 0;
+    for(let i = 0; i < here.length; i++)
+    {
+        if(day - here[i].day > 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 function calculate(here, next, score, day)
 {
-    let selectedNum = pickNum(here.length);
-    let dayDistance = day - here[selectedNum].day
-    let today;
-
-    if(dayDistance < 2 && day >= 2) // 근무와 근무 사이 최소 1일 틈을 주기 위해
+    let pass = 0;
+    let randomPerson = 0;
+    let len = here.length;
+    if(day === 0)
     {
-        selectedNum = pickNum(here.length);
+        len -= 16; // 저번주 일요일에 근무 한 사람 뺌
+    }
+    
+    while(pass < 1)
+    {
+        randomPerson = Math.floor(Math.random() * len - 1) + 1; // Pick random number
+        let dayDistance = day - here[randomPerson].day
+        if(dayDistance < 2 && day >= 2)
+        {
+            len = checkMembersDay(here, day);
+            pass = 0;
+        }
+        else
+        {
+            pass = 1;
+        }
     }
 
-    today = here[selectedNum].name;
+    let today = here[randomPerson].name;
     shiftCount++; ////
-    here[selectedNum].score += score;
-    here[selectedNum].day = day;
-    here[selectedNum].count++;
-    next.push(here[selectedNum]);
-    here.splice(selectedNum, 1);
+    here[randomPerson].score += score;
+    here[randomPerson].day = day;
+    here[randomPerson].count++;
+    next.push(here[randomPerson]);
+    here.splice(randomPerson, 1);
     return today;
 }
+
+loadMembers();
 
 for(let i = 0; i < week; i++)
 {
